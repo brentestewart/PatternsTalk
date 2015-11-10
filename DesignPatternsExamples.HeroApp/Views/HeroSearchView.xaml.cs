54e403570f5f1c162ade7c40d99af01abd115639
Brent Stewart <stewart.brent@gmail.com>
2015-11-10 15:50:56 -0600

    Cleared View Code Behind

--- DesignPatternsExamples.HeroApp/Views/HeroSearchView.xaml.cs
+++ DesignPatternsExamples.HeroApp/Views/HeroSearchView.xaml.cs
@@ -21,46 +21,10 @@ namespace DesignPatternsExamples.HeroApp.Views
 
 		private void ApiButton_OnClick(object sender, RoutedEventArgs e)
 		{
-			var servicePath = Properties.Settings.Default.WebApiUrl;
-			try
-			{
-				using (var client = new HttpClient())
-				{
-					client.BaseAddress = new Uri(servicePath);
-					client.DefaultRequestHeaders.Accept.Clear();
-					client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-					var response = client.GetAsync("api/Heroes").Result;
-					if (response.IsSuccessStatusCode)
-					{
-						var content = response.Content.ReadAsStringAsync().Result;
-					 	HeroList.ItemsSource = JsonConvert.DeserializeObject<List<Hero>>(content);
-						return;
-					}
-				}
-
-			}
-			catch (Exception)
-			{
-				// service is not available
-			}
-
-			HeroList.ItemsSource = new List<Hero>() { new Hero() { HeroName = "Service down..." } };
 		}
 
 		private void JsonButton_OnClick(object sender, RoutedEventArgs e)
 		{
-					var filePath = Properties.Settings.Default.HeroFilePath;
-			if (File.Exists(filePath))
-			{
-				using (var reader = new StreamReader(filePath))
-				{
-					var contents = reader.ReadToEnd();
-					HeroList.ItemsSource = JsonConvert.DeserializeObject<List<Hero>>(contents);
-					return;
-				}
-			}
-
-			HeroList.ItemsSource = new List<Hero> { new Hero { HeroName = "File not found" } };
 		}
 
 		private void ClearButton_OnClick(object sender, RoutedEventArgs e)
